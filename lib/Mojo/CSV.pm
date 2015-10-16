4f31a13bab2a619baf720d37d337840e9823fb7f
Zoffix Znet <cpan@zoffix.com>
2015-10-16 18:38:07 -0400

    First full commit

--- lib/Mojo/CSV.pm
+++ lib/Mojo/CSV.pm
@@ -1,11 +1,121 @@
 package Mojo::CSV;
 
-use Mojo::Base -base;
+use Carp qw/croak/;
+use Mojo::Collection qw/c/;
 use Text::CSV;
+use Scalar::Util qw/reftype/;
+
+use Moo;
+use strictures 2;
+use namespace::clean;
 
 # VERSION
 
+has in  => (
+    is      => 'rw',
+    coerce => sub { __get_fh( $_[0], '<' ); }
+);
+has out  => (
+    is      => 'rw',
+    coerce => sub { __get_fh( $_[0], '>' ); }
+);
+has _obj => (
+    is      => 'ro',
+    default => sub { Text::CSV_XS->new({ binary => 1 }) },
+);
 
+sub __get_fh {
+    my ( $what, $how ) = @_;;
+
+    my $fh;
+    if ( ref $what eq 'Mojo::Asset::File' ) {
+        $fh = $what->handle;
+    }
+    elsif ( ref $what eq 'Mojo::Asset::Memory' ) {
+        open $fh, $how, \ $what->slurp or die $!;
+    }
+    elsif ( ref $what ) {
+        $fh = $what;
+    }
+    elsif ( defined $what ) {
+        open $fh, $how, $what or croak "Failed to open CSV file ($what): $!";
+    }
+    else {
+        croak 'Could not figure out how to access resource';
+    }
+
+    return $fh;
+}
+
+sub flush { close shift->out };
+
+sub row {
+    my $self = shift;
+    my $in = $self->in or croak 'You must specify what to read from';
+    my $row = $self->_obj->getline( $in ) or return;
+    return $row;
+}
+
+sub slurp {
+    my $self = shift;
+    @_ and $self->in( shift );
+
+    my @rows;
+    while ( my $r = $self->row ) {
+        push @rows, $r;
+    }
+
+    return c @rows;
+}
+
+sub slurp_body {
+    my $self = shift;
+    my $r = $self->slurp( @_ );
+    return $r->slice( 1 .. $r->size-1 );
+}
+
+sub spurt {
+    my $self = shift;
+    my $what = shift;
+    @_ and $self->out( shift );
+    $self->trickle( $_ ) for @$what;
+
+    $self;
+}
+
+sub text {
+    my $self = shift;
+    my $what = shift or return '';
+
+    my @out;
+    my $obj = $self->_obj;
+    if (    reftype $what
+            and reftype $what eq 'ARRAY'
+            and reftype $what->[0]
+            and reftype $what->[0] eq 'ARRAY'
+    ) {
+        for my $row ( @$what ) {
+            $obj->combine( @$row ) or next;
+            push @out, $obj->string;
+        }
+    }
+    else {
+        $obj->combine( @$what ) and push @out, $obj->string;
+    }
+
+    return join "\n", @out;
+}
+
+sub trickle {
+    my $self = shift;
+    my $what = shift or return $self;
+    my $out = $self->out or croak 'You must specify where to write';
+    my $obj = $self->_obj;
+    $obj->combine( @$what ) or return $self;
+    print $out $obj->string . "\n";
+
+    $self;
+}
 
 q|
     Computers are like air conditioners.
@@ -20,18 +130,38 @@ __END__
 
 =head1 NAME
 
-Mojo::CSV - no nonsense CSV operations
+Mojo::CSV - no-nonsense CSV handling
 
 =head1 SYNOPSIS
 
 =for pod_spiffy start code section
 
+    # Just convert data to CSV and gimme it
+    say Mojo::CSV->new->text([
+        [ qw/foo bar baz/ ],
+        [ qw/moo mar mer/ ],
+    ]);
+
+    my $data = Mojo::CSV->new->slurp('file.csv') # returns a Mojo::Collection
+        ->grep(sub{ $_->[0] eq 'Zoffix' });
+
+    my $csv = Mojo::CSV->new( in => 'file.csv' );
+    while ( my $row = $csv->row ) {
+        # Read row-by-row
+    }
+
+    # Write CSV file in one go
+    Mojo::CSV->new->spurt( $data => 'file.csv' );
+
+    # Write CSV file row-by-row
+    my csv = Mojo::CSV->new( out => 'file.csv' );
+    $csv->trickle( $row ) for @data;
 
 =for pod_spiffy end code section
 
 =head1 DESCRIPTION
 
-Mojotastic, no-nonsense CSV handling.
+Read and write CSV (Comma Separated Values) like a boss, Mojo style.
 
 =head1 METHODS
 
@@ -39,10 +169,97 @@ Unless otherwise indicated, all methods return their invocant.
 
 =head2 C<new>
 
-    my $pdf = Mojo::PDF->new('myawesome.pdf');
+    Mojo::CSV->new;
+    Mojo::CSV->new( in => 'file.csv', out => 'file.csv' );
 
-Creates a new C<Mojo::PDF> object. Takes one mandatory argument: the filename
-of the PDF you want to generate.
+Creates a new C<Mojo::CSV> object. Takes two optional arguments. See
+L</in> and L</out> for details.
+
+=head2 C<flush>
+
+    $csv->flush;
+
+Closes L</out> filehandle. Call this when you're done L<spurting|/spurt> or
+L<trickling|/trickle> data. This will be done automatically when the
+C<Mojo::CSV> object is destroyed.
+
+=head2 C<in>
+
+    my $csv = Mojo::CSV->new( in => 'file.csv' );
+
+    $csv->in('file.csv');
+
+    open my $fh, '<', 'foo.csv' or die $!;
+    $csv->in( $fh );
+    $csv->in( Mojo::Asset::File->new(path => 'file.csv') );
+    $csv->in( Mojo::Asset::Memory->new->add_chunk('foo,bar,baz') );
+
+Specifies the input for L</slurp> and L</row>. Takes a filename,
+an opened filehandle, a L<Mojo::Asset::File> object, or
+a L<Mojo::Asset::Memory> object.
+
+=head2 C<out>
+
+    my $csv = Mojo::CSV->new( out => 'file.csv' );
+
+    $csv->out('file.csv');
+
+    open my $fh, '>', 'foo.csv' or die $!;
+    $csv->out( $fh );
+
+Specifies the output for L</spurt> and L</trickle>. Takes either
+a filename or an opened filehandle.
+
+=head2 C<row>
+
+    my $row = $csv->row;
+
+Returns an arrayref, which is a row of data from the CSV. The thing
+to read must be first set with L<in method|/in> or C<in> argument to
+L</new>
+
+=head2 C<slurp>
+
+    my $data = Mojo::CSV->new->slurp('file.csv');
+    my $data = Mojo::CSV->new( in => 'file.csv' )->slurp;
+
+Returns a L<Mojo::Collection> object, each item of which is an arrayref
+representing a row of CSV data.
+
+=head2 C<slurp_body>
+
+    my $data = Mojo::CSV->new->slurp_body('file.csv');
+
+A shortcut for calling L</slurp> and discarding the first row
+(use this for CSV with headers you want to get rid of).
+
+=head2 C<spurt>
+
+    Mojo::CSV->new->spurt( $data => 'file.csv');
+    Mojo::CSV->new( out => 'file.csv' )->spurt( $data );
+
+Writes a data structure into CSV. C<$data> is an arrayref of rows, which
+are themselves arrayrefs (each item is a cell data).
+
+=head2 C<text>
+
+    say Mojo::CSV->new->text([qw/foo bar baz/]);
+
+    say Mojo::CSV->new->text([
+        [ qw/foo bar baz/ ],
+        [ qw/moo mar mer/ ],
+    ]);
+
+Returns a CSV string. Takes either a single arrayref to encode just a single
+row or an arrayref of arrayrefs to include multiple rows.
+L<Arrayref-like things|Mojo::Collection> should work too.
+
+=head2 C<trickle>
+
+    my $csv = Mojo::CSV->new( out => 'file.csv' );
+    $csv->trickle( $_ ) for @data;
+
+Writes out a single row of CSV. Takes an arrayref of cell values.
 
 =head1 SEE ALSO
 
--- t/01-csv.t
+++ t/01-csv.t
@@ -1,10 +1,175 @@
 #!perl
 
-use Test::More;
+use Test::Most;
+use Mojo::Asset::File;
+use Mojo::Asset::Memory;
+use Mojo::Util qw/slurp/;
+use File::Temp ();
 
-ok 1;
+use constant CSV_FILE => 't/sample.csv';
+-r CSV_FILE or die 'Failed to find ' . CSV_FILE . ' or it is not readable';
+
+use constant CSV_TEMP => time() . '_temp.csv';
+END { unlink CSV_TEMP };
+
+{ # Init
+    use_ok 'Mojo::CSV';
+    can_ok 'Mojo::CSV',
+        qw/new  slurp  slurp_body  row  spurt  text  trickle  in  out  flush/;
+}
+
+{ # Slurping CSV
+    my $expected = sample_csv();
+    my @in = (
+        [ CSV_FILE, 'filename' ],
+        [ do { open my $fh, '<', CSV_FILE or die $!; $fh }, 'filehandle' ],
+        [
+            Mojo::Asset::File->new(path => CSV_FILE),
+            'Mojo::Asset::File',
+        ],
+        [
+            Mojo::Asset::Memory->new->add_chunk( slurp CSV_FILE ),
+            'Mojo::Asset::Memory',
+        ],
+    );
+
+    is_deeply Mojo::CSV->new->slurp( $_->[0] )->to_array, $expected,
+        "->slurp( arg ) with $_->[1] gives right results"
+            for @in;
+
+    is_deeply Mojo::CSV->new->slurp_body( $_->[0] )->to_array,
+        [ @$expected[1.. $#$expected] ],
+        "->slurp_body with $_->[1] gives right results"
+            for @in[0,3]; # skip filehandles because SEEK position is off
+}
+
+{   # Reading line-by-line
+    my $expected = sample_csv();
+    my $csv = Mojo::CSV->new( in => CSV_FILE );
+    is_deeply $csv->row, $expected->[$_], '->row correct on line ' . $_
+        for 0 .. $#$expected;
+
+    ok ! defined $csv->row, 'another ->row gives undef';
+
+    throws_ok { Mojo::CSV->new->row }
+        qr/You must specify what to read/,
+        'croaks when nothing to read lines from was given';
+}
+
+{ # Spurting CSV
+    unlink CSV_TEMP;
+    Mojo::CSV->new( out => CSV_TEMP )->spurt( sample_csv() );
+    is slurp(CSV_TEMP), slurp(CSV_FILE), '->spurt file matches expectations';
+}
+
+{ # Writing line-by-line
+    unlink CSV_TEMP;
+    my $sample = sample_csv();
+    my $csv = Mojo::CSV->new( out => CSV_TEMP );
+    $csv->trickle( $_ ) for @$sample;
+    $csv->flush;
+    is slurp(CSV_TEMP), slurp(CSV_FILE), '->trickle file matches expectations';
+
+    throws_ok { Mojo::CSV->new->trickle( 42 ) }
+        qr/You must specify where to write/,
+        'croaks when nothing to write lines to was given';
+}
+
+{ # Text
+    my $sample = sample_csv();
+    my $csv = Mojo::CSV->new;
+    chomp( my $expected = slurp(CSV_FILE) );
+    is $csv->text($sample), $expected, '->text on rows matches expectations';
+
+    my @lines = split m{$/}, $expected;
+    is $csv->text($sample->[$_]),
+        $lines[$_], '->text on single row matches expectations on row ' . $_
+            for 0 .. $#$sample;
+}
 
 done_testing;
 
-__END__
-
+sub sample_csv {
+    return [
+          [
+            'order date',
+            'customer id',
+            'customer first_name',
+            'customer last_name',
+            'order number',
+            'item name',
+            'item manufacturer',
+            'item price'
+          ],
+          [
+            '2013-02-01 12:32:00',
+            '7',
+            'Publius',
+            'Ovidius',
+            '23',
+            'fountain pen',
+            'acme',
+            '$3.25'
+          ],
+          [
+            '2013-02-01 12:32:00',
+            '7',
+            'Publius',
+            'Ovidius',
+            '23',
+            'journal',
+            'acme',
+            '$5.50'
+          ],
+          [
+            '2013-02-01 13:01:00',
+            '22',
+            'John',
+            'Davidson ',
+            '42',
+            'journal',
+            'acme',
+            '$5.50'
+          ],
+          [
+            '2013-02-02 10:19:53',
+            '401',
+            'Christina',
+            'Rosetti',
+            '991-2',
+            'journal',
+            'acme',
+            '$5.50'
+          ],
+          [
+            '2013-02-02 09:00:00',
+            '19',
+            'John ',
+            'Davidson',
+            '29',
+            'journal',
+            'PaperWorks',
+            '$4.50'
+          ],
+          [
+            '2013-02-05 19:23:04',
+            '19',
+            'John',
+            'Davidson',
+            '53-1',
+            'pen, ball point',
+            'acme',
+            '$.99'
+          ],
+          [
+            '2013-02-02 10:19:53',
+            '401',
+            'Christina',
+            'Roseti',
+            '13',
+            'journal',
+            'acme',
+            '$5.50'
+          ]
+    ];
+}
